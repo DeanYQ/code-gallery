78d37f18b545183d7dc068e73b7f934d6cbaad6f
DeanYQ <casterd@sina.com>
2017-08-01 15:43:57 +0800

    add the spell check

---
+++ CSharp/SpellCheck/INSTRUCTION.md
@@ -0,0 +1,15 @@
+# Spell Check -- NHunspell
+
+## NuGET Package Command
+Install-Package NHunspell -Version 1.2.5554.16953
+
+[Link] https://www.nuget.org/packages/NHunspell/
+
+## Official Webite
+
+[Main Page] http://www.crawler-lib.net/nhunspell
+[Example Page] http://www.crawler-lib.net/csharp-code-samples-nhunspell
+
+## Other Link -- To Get the dictionary
+
+https://github.com/ropensci/hunspell/tree/master/inst/dict
---
+++ CSharp/SpellCheck/WpfApplication11/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using NHunspell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            using (Hunspell hunspell = new Hunspell("en_us.aff", "en_us.dic"))
+            {
+                var input = txtInput.Text;
+                List<string> suggestions = hunspell.Suggest(input);
+                var sb = new StringBuilder();
+                foreach (string suggestion in suggestions)
+                {
+                    sb.AppendLine(suggestion);
+                }
+                this.txtOutput.Text = sb.ToString();
+            }
+        }
+    }
+}
